b582fb5caede2fefd6db705f1f045db731d883f2
Lauren Yu <lauren.yu@datasociety.com>
2023-05-26 13:01:24 -0400

    added logic for todo reducer. Needs to be implemented in later steps.

---
+++ src/context/Reducers.ts
@@ -0,0 +1,26 @@
+import { Todo } from "../model";
+
+type Actions = 
+| { type: "add"; payload: string }
+| { type: "remove"; payload: number }
+| { type: "done"; payload: number };
+
+export const TodoReducer = (state: Todo[], actions: Actions) => {
+    switch (actions.type) {
+        case "add":
+            return [
+                ...state,
+                { id: Date.now(), todo: actions.payload, isDone: false}
+            ]
+        case "remove":
+            return state.filter((todo) => todo.id !== actions.payload)
+        case "done":
+            return state.map((todo) => 
+                todo.id !== actions.payload? {...todo, isDone: !todo} : todo
+            )
+        default:
+            return state
+    }
+};
+
+// const [state, dispatch] = useReducer(TodoReducer, [])
